778727f7b3806034c802c5c1a23f77549e961018
atakanbue <atakanb25@gmail.com>
2026-06-03 18:23:04 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+{
+  "name": "notenvergabe-survey",
+  "version": "1.0.0",
+  "description": "Bachelor-Projekt Fragebogen zur Notenvergabe",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "csv-stringify": "^6.5.2",
+    "express": "^4.18.3",
+    "pg": "^8.11.5"
+  }
+}
